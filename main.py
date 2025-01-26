665e06a52e2acb05bbf4e6fe33a98933aa14a26e
TJTJ-cpu <117895770+TJTJ-cpu@users.noreply.github.com>
2025-01-26 15:44:56 +0100

    Feature: Data can now be exported to excel

--- main.py
+++ main.py
@@ -7,11 +7,12 @@ baseUrl = 'https://api.openf1.org/v1/position?'
 meeting_code = 1244
 meeting_key = f'meeting_key={meeting_code}'
 
-driverNumber = 16
+driverNumber = 1
 driverKey = f'driver_number={driverNumber}'
 
 sessionCode = 9590
 sessionKey = f'session_key={sessionCode}'
+
 
 
 finalUrl = f'{baseUrl}&{meeting_key}&{driverKey}&{sessionKey}'
@@ -19,6 +20,9 @@ finalUrl = f'{baseUrl}&{meeting_key}&{driverKey}&{sessionKey}'
 response = urlopen(finalUrl)
 data = json.loads(response.read().decode('utf-8'))
 df = pd.DataFrame(data)
+
+outputFile = 'F1Data.xlsx'
+df.to_excel(outputFile, index=False)
 
 print(df)
 
